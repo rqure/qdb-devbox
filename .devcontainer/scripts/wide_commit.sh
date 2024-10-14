0ec003c655373c02061ebf74037f9a3e45150cb3
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-10-14 01:30:13 +0000

    Add wide commit script

---
+++ .devcontainer/scripts/wide_commit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+REPOS=(
+  "https://github.com/rqure/qdb.git"
+  "https://github.com/rqure/qdeploy.git"
+  "https://github.com/rqure/qschematic.git"
+  "https://github.com/rqure/qwebgateway.git"
+  "https://github.com/rqure/qgarage.git"
+  "https://github.com/rqure/qadhan.git"
+  "https://github.com/rqure/qalert.git"
+  "https://github.com/rqure/qmqttgateway.git"
+  "https://github.com/rqure/qaudio-player.git"
+  "https://github.com/rqure/qclock.git"
+  "https://github.com/rqure/qsmtp.git"
+  "https://github.com/rqure/qlogger.git"
+  "https://github.com/rqure/qdb-cli.git"
+  "https://github.com/rqure/qsm.git"
+)
+
+for REPO in "${REPOS[@]}"; do
+    REPO_NAME=$(basename $REPO .git)
+    (cd $REPO_NAME && git commit -am "$1" && git push)
+done
