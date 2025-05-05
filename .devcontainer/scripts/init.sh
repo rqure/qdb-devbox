b7041e3b01921c3dc79635bc7d303935b087ca26
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2025-05-05 17:05:24 +0000

    Fixing SchemaUpdate

--- .devcontainer/scripts/init.sh
+++ .devcontainer/scripts/init.sh
@@ -2,19 +2,7 @@
 
 REPOS=(
   "https://github.com/rqure/qlib.git"
-  "https://github.com/rqure/qdeploy.git"
-  "https://github.com/rqure/qschematic.git"
   "https://github.com/rqure/qcore.git"
-  "https://github.com/rqure/qgarage.git"
-  "https://github.com/rqure/qadhan.git"
-  "https://github.com/rqure/qalert.git"
-  "https://github.com/rqure/qmqttgateway.git"
-  "https://github.com/rqure/qaudio-player.git"
-  "https://github.com/rqure/qclock.git"
-  "https://github.com/rqure/qsmtp.git"
-  "https://github.com/rqure/qdb-cli.git"
-  "https://github.com/rqure/qsm.git"
-  "https://github.com/rqure/qdp.git"
 )
 
 # Clone each repository if it doesn't already exist
