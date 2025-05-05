fe404d7837de20e67954f5b34beb17c67dc074c8
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2025-05-05 22:33:49 +0000

    some progress

--- .devcontainer/scripts/commit_all.sh
+++ .devcontainer/scripts/commit_all.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
 REPOS=(
-  "https://github.com/rqure/qdb.git"
   "https://github.com/rqure/qlib.git"
-  "https://github.com/rqure/qdeploy.git"
-  "https://github.com/rqure/qschematic.git"
   "https://github.com/rqure/qcore.git"
-  "https://github.com/rqure/qgarage.git"
-  "https://github.com/rqure/qadhan.git"
-  "https://github.com/rqure/qalert.git"
-  "https://github.com/rqure/qmqttgateway.git"
-  "https://github.com/rqure/qaudio-player.git"
-  "https://github.com/rqure/qclock.git"
-  "https://github.com/rqure/qsmtp.git"
-  "https://github.com/rqure/qdb-cli.git"
-  "https://github.com/rqure/qsm.git"
-  "https://github.com/rqure/qdp.git"
 )
 
 for REPO in "${REPOS[@]}"; do
--- .devcontainer/scripts/tag_all.sh
+++ .devcontainer/scripts/tag_all.sh
@@ -2,15 +2,6 @@
 
 REPOS=(
   "https://github.com/rqure/qcore.git"
-  "https://github.com/rqure/qgarage.git"
-  "https://github.com/rqure/qadhan.git"
-  "https://github.com/rqure/qalert.git"
-  "https://github.com/rqure/qmqttgateway.git"
-  "https://github.com/rqure/qaudio-player.git"
-  "https://github.com/rqure/qclock.git"
-  "https://github.com/rqure/qsmtp.git"
-  "https://github.com/rqure/qsm.git"
-  "https://github.com/rqure/qdp.git"
 )
 
 for REPO in "${REPOS[@]}"; do
